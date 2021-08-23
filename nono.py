d153d5575d9b9d29cd711ca3bdc86c79d7506f5c
David Winn <q@qypea.com>
2021-08-23 15:43:11 -0700

    Convert to use numpy to store state, print it cleanly

--- nono.py
+++ nono.py
@@ -1,25 +1,117 @@
 #!/usr/bin/env python3
 
+import enum
+import numpy
 import unittest
 
-def parse_row(state: str) -> list:
+
+class Square(enum.IntEnum):
+    """Enum for the state of a single square."""
+    UNKNOWN = 0
+    EMPTY = 1
+    FULL = 2
+
+
+class Nonogram:
+    """Collection of the data required to represent a nonogram."""
+    def __init__(self, row_size, col_size, row_inputs, col_inputs):
+        self.array = numpy.ndarray([row_size, col_size],
+                                   dtype=numpy.ubyte,
+                                   order='C')
+        self.array.fill(Square.UNKNOWN)
+
+        self.row_inputs = row_inputs
+        self.col_inputs = col_inputs
+        self._row_labels = []
+        self._row_pad = ""
+        self._col_labels = []
+        self._max_col_labels = 0
+
+        self._gen_labels()
+
+    def _gen_labels(self):
+        """Generate the row and column labels for printing."""
+        # Build up the row labels
+        for row_input in self.row_inputs:
+            label = ""
+            for i in row_input:
+                label += "{0: <2} ".format(i)
+            self._row_labels.append(label)
+
+        max_label = max([len(x) for x in self._row_labels])
+        for i, label in zip(range(len(self._row_labels)), self._row_labels):
+            while len(label) < max_label:
+                label = " " + label
+            self._row_labels[i] = label
+
+        self._row_pad = " " * max_label
+
+        # Build up column labels
+        self._max_col_labels = max([len(label) for label in self.col_inputs])
+        for column in self.col_inputs:
+            self._col_labels.append(["{0: <2} ".format(label) for label in column])
+
+        for label in self._col_labels:
+            while len(label) < self._max_col_labels:
+                label.insert(0, "   ")
+
+    def __str__(self):
+        """Print our state prettily."""
+        ret = ""
+
+        # Columns
+        for i in range(self._max_col_labels):
+            ret += self._row_pad
+            for label in self._col_labels:
+                ret += label[i]
+            ret += "\n"
+
+        # Actual data rows
+        for row_label, row in zip(self._row_labels, self.array):
+            ret += row_label
+            for item in row:
+                if item == Square.UNKNOWN:
+                    ret += " "
+                elif item == Square.EMPTY:
+                    ret += "X"
+                elif item == Square.FULL:
+                    ret += "#"
+                else:
+                    raise InvalidValue("Unexpected square value to print")
+                ret += "  "
+            ret += "\n"
+
+        return ret
+
+    def row(self, i):
+        """Return the row i."""
+        return self.row_inputs[i], self.array[i]
+
+    def column(self, i):
+        """Return the column i."""
+        return self.col_inputs[i], self.array[:,i]
+
+
+def parse_row(state: list) -> list:
     """Given a state extract the inputs for that state."""
-    assert " " not in state, "No empty squares allowed"
+    assert Square.UNKNOWN not in state, "No empty squares allowed"
 
-    if state == "":
+    if not state:
         return []
 
-    start = state.find("#")
-    if start == -1:
+    try:
+        start = state.index(Square.FULL)
+    except ValueError:
         return []
 
-    end = state.find("x", start)
-    if end == -1:
-        length = len(state[start:])
-        rest = ""
-    else:
+    try:
+        end = state.index(Square.EMPTY, start)
+
         length = len(state[start:end])
         rest = state[end:]
+    except ValueError:
+        length = len(state[start:])
+        rest = []
 
     substate = parse_row(rest)
 
@@ -30,40 +122,40 @@ class TestParseRow(unittest.TestCase):
     """Test parse_row function."""
     def test_parse_row(self):
         """Test parse_row function."""
-        cases = [("", []),
-                 ("x", []),
-                 ("#", [1]),
+        cases = [([], []),
+                 ([1], []),
+                 ([2], [1]),
 
-                 ("xx", []),
-                 ("#x", [1]),
-                 ("x#", [1]),
-                 ("##", [2]),
+                 ([1, 1], []),
+                 ([2, 1], [1]),
+                 ([1, 2], [1]),
+                 ([2, 2], [2]),
 
-                 ("xxx", []),
-                 ("#xx", [1]),
-                 ("x#x", [1]),
-                 ("##x", [2]),
-                 ("xx#", [1]),
-                 ("#x#", [1, 1]),
-                 ("x##", [2]),
-                 ("###", [3]),
+                 ([1, 1, 1], []),
+                 ([2, 1, 1], [1]),
+                 ([1, 2, 1], [1]),
+                 ([2, 2, 1], [2]),
+                 ([1, 1, 2], [1]),
+                 ([2, 1, 2], [1, 1]),
+                 ([1, 2, 2], [2]),
+                 ([2, 2, 2], [3]),
 
-                 ("xxxx", []),
-                 ("#xxx", [1]),
-                 ("x#xx", [1]),
-                 ("##xx", [2]),
-                 ("xx#x", [1]),
-                 ("#x#x", [1, 1]),
-                 ("x##x", [2]),
-                 ("###x", [3]),
-                 ("xxx#", [1]),
-                 ("#xx#", [1, 1]),
-                 ("x#x#", [1, 1]),
-                 ("##x#", [2, 1]),
-                 ("xx##", [2]),
-                 ("#x##", [1, 2]),
-                 ("x###", [3]),
-                 ("####", [4]),
+                 ([1, 1, 1, 1], []),
+                 ([2, 1, 1, 1], [1]),
+                 ([1, 2, 1, 1], [1]),
+                 ([2, 2, 1, 1], [2]),
+                 ([1, 1, 2, 1], [1]),
+                 ([2, 1, 2, 1], [1, 1]),
+                 ([1, 2, 2, 1], [2]),
+                 ([2, 2, 2, 1], [3]),
+                 ([1, 1, 1, 2], [1]),
+                 ([2, 1, 1, 2], [1, 1]),
+                 ([1, 2, 1, 2], [1, 1]),
+                 ([2, 2, 1, 2], [2, 1]),
+                 ([1, 1, 2, 2], [2]),
+                 ([2, 1, 2, 2], [1, 2]),
+                 ([1, 2, 2, 2], [3]),
+                 ([2, 2, 2, 2], [4]),
                 ]
         for state, expected_input in cases:
             with self.subTest(state=state):
